0c7d3481ab5463545f92e8185ac2805a2b59fcea
NikitaServaisky <servaiski@gmail.com>
2024-08-10 14:54:46 +0300

    git init repository

---
+++ frontend/src/App.jsx
@@ -0,0 +1,55 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorElement from './Pagas/ErrorElement';
+import Root from './Pagas/Root';
+import Home from './Pagas/Home';
+import About from './Pagas/About';
+import Account from './Pagas/Account';
+import Contacts from './Pagas/Contats';
+import Products from './Pagas/Products';
+import Cart from './Pagas/Cart';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorElement />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/account',
+        element: <Account />,
+      },
+      {
+        path: '/products',
+        element: <Products />,
+      },
+      {
+        path: '/contact',
+        element: <Contacts />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+    ],
+  },
+
+  {
+    path: '*',
+    element: <ErrorElement />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
